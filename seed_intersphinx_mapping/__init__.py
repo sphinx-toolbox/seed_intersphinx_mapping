571424479230b8c486750e4ac18ae5cca3e3eee5
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-08-18 22:24:33 +0100

    Bump version: 0.0.0 → 0.0.1

--- seed_intersphinx_mapping/__init__.py
+++ seed_intersphinx_mapping/__init__.py
@@ -35,5 +35,5 @@ __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020 Dominic Davis-Foster"
 
 __license__: str = "MIT License"
-__version__: str = "0.0.0"
+__version__: str = "0.0.1"
 __email__: str = "dominic@davis-foster.co.uk"
