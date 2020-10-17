08b6ea6bf6d29a4885a63b8d64f36655d1f9e571
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-10-17 16:23:15 +0100

    Updated config files.

--- setup.py
+++ setup.py
@@ -12,13 +12,10 @@ sys.path.append('.')
 # this package
 from __pkginfo__ import *  # pylint: disable=wildcard-import
 
-
-
 setup(
 		description="Populate the Sphinx 'intersphinx_mapping' dictionary from the project's requirements.",
 		extras_require=extras_require,
 		install_requires=install_requires,
 		py_modules=[],
 		version=__version__,
-
 		)
