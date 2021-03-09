70ac839583dc5f468278eeea0338548949d8ab27
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-03-09 22:16:08 +0000

    Add more projects to the fallback mapping as they don't advertise their docs in their metadata.

---
+++ check_packages.py
@@ -0,0 +1,30 @@
+"""
+Checks the top 4000 packages on PyPI to see whether they have a documentation URL that
+``seed_intersphinx_mapping`` can understand.
+"""
+
+# 3rd party
+# Download hugovk's list of top 4000 most downloaded packages
+import requests
+
+# this package
+from seed_intersphinx_mapping import get_sphinx_doc_url
+
+url = "https://hugovk.github.io/top-pypi-packages/top-pypi-packages-30-days.json"
+top_packages = [p["project"] for p in requests.get(url).json()["rows"]]
+
+print(top_packages)
+
+YES = '✔'
+NO = '✘'
+
+for project in top_packages:
+	print(project, end='')
+
+	try:
+		doc_url = get_sphinx_doc_url(project)
+		print(f" {YES} : {doc_url}")
+	except ValueError as e:
+		print(f" {NO} : {e}")
+
+	input(">>>")
