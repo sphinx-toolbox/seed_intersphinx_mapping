3e06bd83e119fb94e27957c4fd1845d501c579cc
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-09-21 22:07:35 +0100

    Work around Sphinx issue with Python 3.10 and UnionType

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,15 @@
 # stdlib
 import pathlib
+import sys
 
 # 3rd party
 import pytest
 from sphinx.testing.path import path
+
+if sys.version_info >= (3, 10):
+	# stdlib
+	import types
+	types.Union = types.UnionType
 
 pytest_plugins = ("sphinx.testing.fixtures", "coincidence")
 
