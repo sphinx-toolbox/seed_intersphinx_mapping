26fd5c73211f7187a5eced6ddc30ecc46a0ee85b
Mathieu <13415583+mathbou@users.noreply.github.com>
2022-03-15 00:48:09 +0100

    ✅ fix core test assertion

--- tests/test_core.py
+++ tests/test_core.py
@@ -24,7 +24,7 @@ def test_get_sphinx_doc_url():
 	with pytest.raises(ValueError, match="Documentation URL not found in data from PyPI."):
 		get_sphinx_doc_url("slumber")
 
-	with pytest.raises(ValueError, match="objects.inv not found at url .*: HTTP Status 404"):
+	with pytest.raises(ValueError, match="Documentation URL not found in data from PyPI."):
 		get_sphinx_doc_url("isort")
 
 	assert cache.clear(get_sphinx_doc_url)
